2ac24099187faf3de8034bc7fcd502363e7a5408
Matt Chastain <work@mattchastain.dev>
2025-03-21 13:14:11 -0400

    import Button, render a "Create Container" button if no containers exist

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { Button } from './components';
 import { useContainerStore } from './lib';
 
 export default function App() {
   const { containers, setContainers, addContainer } = useContainerStore();
 
   return (
-    <div>App</div>
+    <div className='mx-auto max-w-7xl'>
+      {containers.length === 0 && (
+        <div className='fixed inset-0 flex items-center justify-center'>
+          <Button label='Create Container' />
+        </div>
+      )}
+    </div>
   )
 }
