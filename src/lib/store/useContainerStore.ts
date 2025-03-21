45e263b66b3642ce0f790618e01828a1aa5ac6ae
Matt Chastain <work@mattchastain.dev>
2025-03-21 12:12:15 -0400

    create type ContainerState

--- src/lib/store/useContainerStore.ts
+++ src/lib/store/useContainerStore.ts
@@ -1,2 +1,8 @@
 import { create } from 'zustand';
 import { ContainerType } from '..';
+
+type ContainerState = {
+    containers: ContainerType[];
+    setContainer: (title: string) => void;
+    addContainer: (title: string) => void;
+}
