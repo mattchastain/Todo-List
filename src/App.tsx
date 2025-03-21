b161bb50162cb4ff092040d1743e8937e1782bfe
Matt Chastain <work@mattchastain.dev>
2025-03-21 14:37:53 -0400

    imprort useState, add onClick to create container button

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
+import { CirclePlus } from 'lucide-react';
 import { Button } from './components';
 import { useContainerStore } from './lib';
-import { CirclePlus } from 'lucide-react';
+
+import { useState } from 'react';
 
 export default function App() {
-  const { containers, setContainers, addContainer } = useContainerStore();
+	const { containers, setContainers, addContainer } = useContainerStore();
+	const [showCreateContainerModal, setShowCreateContainerModal] =
+		useState(false);
 
-  return (
-    <div className='mx-auto max-w-7xl'>
-      {containers.length === 0 && (
-        <div className='fixed inset-0 flex items-center justify-center'>
-          <Button label='Create Container' icon={CirclePlus} />
-        </div>
-      )}
-    </div>
-  )
+	return (
+		<div className='mx-auto max-w-7xl'>
+			{containers.length === 0 && (
+				<div className='fixed inset-0 flex items-center justify-center'>
+					<Button
+						label='Create Container'
+						icon={CirclePlus}
+						onClick={() => setShowCreateContainerModal(true)}
+					/>
+				</div>
+			)}
+		</div>
+	);
 }
