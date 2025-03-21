a68be9508abdbd4a5b9cfa27ff4f9ac145971ae8
Matt Chastain <work@mattchastain.dev>
2025-03-21 13:49:29 -0400

    create ModalProps interface

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,6 +1,12 @@
 import FocusTrap from 'focus-trap-react';
 import { AnimatePresence, motion } from 'framer-motion';
 
-export function modal() {
+interface ModalProps {
+	showModal: boolean;
+	setShowModal: (value: boolean) => void;
+	children: React.ReactNode;
+}
+
+export function Modal({ showModal, setShowModal, children }: ModalProps) {
 	return <div>modal</div>;
 }
