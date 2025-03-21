32c4722881ae0a4ff988e56c05915e0fb2af8b7f
Matt Chastain <work@mattchastain.dev>
2025-03-21 12:24:16 -0400

    destructure useContainerStore

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useContainerStore } from './lib';
 
 export default function App() {
+  const { containers, setContainers, addContainer } = useContainerStore();
+
   return (
     <div>App</div>
   )
