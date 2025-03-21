2fd1b3a41ed0cf5d20c04b1a1ad80c240ebd6b2d
Matt Chastain <work@mattchastain.dev>
2025-03-21 16:33:59 -0400

    import components / useState, create h1, Input, Button

--- src/components/addContainerModal/addContainerModal.tsx
+++ src/components/addContainerModal/addContainerModal.tsx
@@ -1,17 +1,31 @@
-import { Modal } from '..';
+import { useState } from 'react';
+import { Button, Input, Modal } from '..';
+import { isContainerNameEmpty } from '../../lib';
 
 interface AddContainerProps {
 	showModal: boolean;
 	setShowModal: (value: boolean) => void;
 }
 
-export function AddContainerModal({showModal, setShowModal}: AddContainerProps) {
+export function AddContainerModal({
+	showModal,
+	setShowModal,
+}: AddContainerProps) {
+	const [containerName, setContainerName] = useState('');
+
 	return (
-		<Modal
-			showModal={showModal}
-			setShowModal={setShowModal}
-		>
-			<div><button></button></div>
+		<Modal showModal={showModal} setShowModal={setShowModal}>
+			<div className='flex flex-col gap-3 w-full bg-neutral-800/50 rounded-md md:rounded-xl p-2 md:p-3 text-center text-xl md:text-2xl'>
+				<h1 className='font-bold'>Add New Container</h1>
+				<Input
+					type='text'
+					name='container name'
+					placeholder='Container name...'
+					value={containerName}
+					onChange={(e) => setContainerName(e.target.value)}
+				/>
+				<Button label='Add Container' fullWidth={true} disabled={isContainerNameEmpty(containerName)} />
+			</div>
 		</Modal>
 	);
 }
