13d3c13bc7eae746746b0103e67b551adf08d6ea
Matt Chastain <work@mattchastain.dev>
2025-03-21 13:14:55 -0400

    import and add CirclePlus icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Button } from './components';
 import { useContainerStore } from './lib';
+import { CirclePlus } from 'lucide-react';
 
 export default function App() {
   const { containers, setContainers, addContainer } = useContainerStore();
@@ -8,7 +9,7 @@ export default function App() {
     <div className='mx-auto max-w-7xl'>
       {containers.length === 0 && (
         <div className='fixed inset-0 flex items-center justify-center'>
-          <Button label='Create Container' />
+          <Button label='Create Container' icon={CirclePlus} />
         </div>
       )}
     </div>
