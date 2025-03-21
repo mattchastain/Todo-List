6c0ebe3afb0e26cad82aacd993e0b0833df32605
Matt Chastain <work@mattchastain.dev>
2025-03-21 19:28:32 -0400

    create DeleteAllContainersProps interface, create export function

---
+++ src/components/deleteAllContainersModal/deleteAllContainersModal.tsx
@@ -0,0 +1,11 @@
+interface DeleteAllContainersProps {
+	showModal: boolean;
+	setShowModal: (value: boolean) => void;
+}
+
+export function DeleteAllContainersModal({
+	showModal,
+	setShowModal,
+}: DeleteAllContainersProps) {
+	return <div></div>;
+}
