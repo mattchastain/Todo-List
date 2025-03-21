0cac77b8b7ca02e483e4beb95000cb353212923c
Matt Chastain <work@mattchastain.dev>
2025-03-21 14:41:55 -0400

    create export function, render empty modal

--- src/components/createContainer/createContainer.tsx
+++ src/components/createContainer/createContainer.tsx
@@ -1,4 +1,17 @@
+import { Modal } from '..';
+
 interface CreateContainerProps {
 	showModal: boolean;
 	setShowModal: (value: boolean) => void;
+}
+
+export function CreateContainer({showModal, setShowModal}: CreateContainerProps) {
+	return (
+		<Modal
+			showModal={showModal}
+			setShowModal={setShowModal}
+		>
+			<div></div>
+		</Modal>
+	);
 }
