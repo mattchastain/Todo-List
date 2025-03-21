ccc9de36eac63d9bf26a7676904afd3bdfd06373
Matt Chastain <work@mattchastain.dev>
2025-03-21 14:17:39 -0400

    create and style modal with AnimatePresence/motion

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,5 +1,6 @@
-import FocusTrap from 'focus-trap-react';
+import { FocusTrap } from 'focus-trap-react';
 import { AnimatePresence, motion } from 'framer-motion';
+import { useCallback, useEffect, useRef } from 'react';
 
 interface ModalProps {
 	showModal: boolean;
@@ -7,6 +8,34 @@ interface ModalProps {
 	children: React.ReactNode;
 }
 
-export function Modal({ showModal, setShowModal, children }: ModalProps) {
-	return <div>modal</div>;
+export function Modal({
+	showModal = true,
+	setShowModal,
+	children,
+}: ModalProps) {
+	return (
+		<AnimatePresence>
+			{showModal && (
+				<>
+					<FocusTrap focusTrapOptions={{ initialFocus: false }}>
+						<motion.div
+							initial={{ scale: 0.7, opacity: 0 }}
+							animate={{ scale: 1, opacity: 1 }}
+							exit={{ scale: 0.7, opacity: 0 }}
+							className='fixed inset-0 flex items-center justify-center z-50 min-h-screen'
+						>
+							<div>{children}</div>
+						</motion.div>
+					</FocusTrap>
+					<motion.div
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+						className='fixed inset-0 flex items-center justify-center z-40 min-h-screen bg-neutral-950/50 backdrop-blur'
+						onClick={() => setShowModal(false)}
+					/>
+				</>
+			)}
+		</AnimatePresence>
+	);
 }
