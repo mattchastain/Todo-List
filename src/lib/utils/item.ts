8d6238ca68f936a7d5385f8bffd165eb259faca1
Matt Chastain <work@mattchastain.dev>
2025-03-24 12:42:10 -0400

    create onAddItem

---
+++ src/lib/utils/item.ts
@@ -0,0 +1,21 @@
+import { ContainerType } from '../types';
+
+export const onAddItem = (
+	itemName: string,
+	setItemName: (name: string) => void,
+	setShowModal: (showModal: boolean) => void,
+	containers: ContainerType[],
+	setContainers: (containers: ContainerType[]) => void,
+	currentContainerId: Date
+) => {
+	if (!itemName) return;
+	const id = new Date();
+	const container = containers.find(
+		(container) => container.id === currentContainerId
+	);
+	if (!container) return;
+	container.items.push({ id, title: itemName, completed: false });
+	setContainers([...containers]);
+	setItemName('');
+	setShowModal(false);
+};
