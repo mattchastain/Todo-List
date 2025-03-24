b117f6fe447e68f81610f447e9152069c97de453
Matt Chastain <work@mattchastain.dev>
2025-03-24 09:13:05 -0400

    export item.tsx

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ export * from './button/button';
 export * from './container/container';
 export * from './deleteAllContainersModal/deleteAllContainersModal';
 export * from './input/input';
+export * from './item/item';
 export * from './modal/modal';
 export * from './toolbar/toolbar';
