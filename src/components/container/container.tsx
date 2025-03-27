01807150ec1c4be0bfe1e7e9ec6cc18ecdbdce0a
Matt Chastain <work@mattchastain.dev>
2025-03-27 11:06:53 -0400

    fix height, remove animation

--- src/components/container/container.tsx
+++ src/components/container/container.tsx
@@ -1,4 +1,3 @@
-import { AnimatePresence, motion } from 'framer-motion';
 import { EllipsisVertical, Pencil, Trash2 } from 'lucide-react';
 import { Button, Dropdown, Item } from '..';
 import { ItemType } from '../../lib';
@@ -19,51 +18,41 @@ export function Container({
 	onDeleteContainer,
 }: ContainerProps) {
 	return (
-		<AnimatePresence>
-			<motion.div
-				initial={{ scale: 0.7, opacity: 0 }}
-				animate={{ scale: 1, opacity: 1 }}
-				exit={{ scale: 0.7, opacity: 0 }}
-			>
-				<div className='flex flex-col gap-3 w-full bg-neutral-900 border border-neutral-800/80 rounded-md md:rounded-xl p-2 md:p-3'>
-					<div className='flex justify-between'>
-						<h1 className='font-bold text-xl md:text-2xl'>
-							{title}
-						</h1>
-						<Dropdown label='' icon={EllipsisVertical}>
-							{/* <Button
+		<div className='flex flex-col gap-3 w-full h-max bg-neutral-900 border border-neutral-800/80 rounded-md md:rounded-xl p-2 md:p-3'>
+			<div className='flex justify-between'>
+				<h1 className='font-bold text-xl md:text-2xl'>{title}</h1>
+				<Dropdown label='' icon={EllipsisVertical}>
+					{/* <Button
 								label='Edit'
 								fullWidth={true}
 								variant='default'
 								icon={Pencil}
 								dropdownItem={true}
 							/> */}
-							<Button
-								label='Delete'
-								onClick={onDeleteContainer}
-								fullWidth={true}
-								variant='danger'
-								icon={Trash2}
-								dropdownItem={true}
-							/>
-						</Dropdown>
-					</div>
-					{items.map((item) => (
-						<Item
-							id={item.id}
-							title={item.title}
-							completed={item.completed}
-							containerId={id}
-						/>
-					))}
 					<Button
-						label='Add Task'
-						onClick={onAddItem}
+						label='Delete'
+						onClick={onDeleteContainer}
 						fullWidth={true}
-						variant='ghost'
+						variant='danger'
+						icon={Trash2}
+						dropdownItem={true}
 					/>
-				</div>
-			</motion.div>
-		</AnimatePresence>
+				</Dropdown>
+			</div>
+			{items.map((item) => (
+				<Item
+					id={item.id}
+					title={item.title}
+					completed={item.completed}
+					containerId={id}
+				/>
+			))}
+			<Button
+				label='Add Task'
+				onClick={onAddItem}
+				fullWidth={true}
+				variant='ghost'
+			/>
+		</div>
 	);
 }
