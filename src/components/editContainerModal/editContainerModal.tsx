6d28cb07b455387a7c2c85803002eef959e4eeb0
Matt Chastain <work@mattchastain.dev>
2025-03-28 13:26:09 -0400

    create onKeyDown, useEffect, and Modal

--- src/components/editContainerModal/editContainerModal.tsx
+++ src/components/editContainerModal/editContainerModal.tsx
@@ -1,3 +1,7 @@
+import { useCallback, useEffect, useState } from 'react';
+import { Button, Input, Modal } from '..';
+import { isContainerNameEmpty } from '../../lib';
+
 interface EditContainerModalProps {
 	containerId: Date | undefined;
 	showModal: boolean;
@@ -13,5 +17,42 @@ export function EditContainerModal({
 	setEditedContainerTitle,
 	onEditContainer,
 }: EditContainerModalProps) {
-	return <div>editContainerModal</div>;
+	const [editedTitle, setEditedTitle] = useState<string>('');
+
+	const onKeyDown = useCallback(
+		(e: KeyboardEvent) => {
+			if (isContainerNameEmpty(editedTitle)) return;
+			if (e.key === 'Enter') onEditContainer();
+		},
+		[editedTitle, setShowModal]
+	);
+
+	useEffect(() => {
+		document.addEventListener('keydown', onKeyDown);
+		return () => document.removeEventListener('keydown', onKeyDown);
+	}, [onKeyDown]);
+
+	return (
+		<Modal showModal={showModal} setShowModal={setShowModal}>
+			<h1 className='font-bold'>Edit Container</h1>
+			<Input
+				type='text'
+				name='new container title'
+				placeholder='New container name...'
+				value={editedTitle}
+				onChange={(e) => {
+					setEditedTitle(e.target.value);
+					setEditedContainerTitle(e.target.value);
+				}}
+			/>
+			<Button
+				label='Edit Container'
+				onClick={() => {
+					onEditContainer();
+				}}
+				fullWidth={true}
+				disabled={isContainerNameEmpty(editedTitle)}
+			/>
+		</Modal>
+	);
 }
