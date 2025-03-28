c66d61585624fe664b0945b1647c1cacc05ceef5
Matt Chastain <work@mattchastain.dev>
2025-03-28 13:13:29 -0400

    add backdrop-blur to dropdown

--- src/components/dropdown/dropdown.tsx
+++ src/components/dropdown/dropdown.tsx
@@ -26,30 +26,43 @@ export function Dropdown({
 	};
 
 	return (
-		<div className='relative'>
-			<Button
-				label=''
-				onClick={() => setIsOpen(!isOpen)}
-				variant='ghost'
-				icon={icon}
-			/>
+		<>
+			<div className={`relative ${isOpen && 'z-20'}`}>
+				<Button
+					label=''
+					onClick={() => setIsOpen(!isOpen)}
+					variant='ghost'
+					icon={icon}
+				/>
+				<AnimatePresence>
+					{isOpen && (
+						<motion.div
+							initial={{ scale: 0.9, opacity: 0 }}
+							animate={{ scale: 1, opacity: 1 }}
+							exit={{ scale: 0.9, opacity: 0 }}
+							className={`absolute bg-neutral-800 border border-neutral-700/80 flex flex-col max-w-3xs gap-2 p-2 rounded-lg md:rounded-xl z-20 ${positionClasses[position]}`}
+						>
+							{label && (
+								<h1 className='text-xs md:text-sm font-bold'>
+									{label}
+								</h1>
+							)}
+							{children}
+						</motion.div>
+					)}
+				</AnimatePresence>
+			</div>
 			<AnimatePresence>
 				{isOpen && (
 					<motion.div
-						initial={{ scale: 0.9, opacity: 0 }}
-						animate={{ scale: 1, opacity: 1 }}
-						exit={{ scale: 0.9, opacity: 0 }}
-						className={`absolute bg-neutral-800 border border-neutral-700/80 flex flex-col max-w-3xs gap-2 p-2 rounded md:rounded-lg z-20 ${positionClasses[position]}`}
-					>
-						{label && (
-							<h1 className='text-xs md:text-sm font-bold'>
-								{label}
-							</h1>
-						)}
-						{children}
-					</motion.div>
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+						className='fixed inset-0 bg-neutral-950/50 backdrop-blur z-10'
+						onClick={() => setIsOpen(false)}
+					></motion.div>
 				)}
 			</AnimatePresence>
-		</div>
+		</>
 	);
 }
